a93123e915ef07b1d5ad2ad241bf69d9da3a916d
Amparo Lopez Gaona <alg@ciencias.unam.mx>
2020-04-20 13:04:59 -0500

    Assignement 3 finished

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 #'makeCacheMatrix'  creates a list containing a function to set/get
 #value of the matrix and get/set the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
